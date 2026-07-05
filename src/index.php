d538c62bc0fba9439c3787c9b1f667c796d552bd
Skarab <skarab47@gmail.com>
2026-07-05 14:03:33 +0300

    Local Docker Project - NGINX, PHP and MYSQL

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php 
+echo "NGINX Online: OK<br>";
+echo "PHP Online: OK<br>";
+
+$host = getenv("DB_HOST");
+$db = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+
+try {
+  $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+  echo "RDS Connection Online: OK<br>";
+} catch (PDOException $e) {
+  echo "RDS Connection FAILED: " . $e->getMessage() . "<br>";
+}
